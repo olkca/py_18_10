2887df99c42504fc2b012e13c80963d0a370c6c0
Shapoval <olekcandrsapoval41@gmail.com>
2022-12-10 23:15:31 +0200

    Ex_4

---
+++ main.py
@@ -0,0 +1,8 @@
+a=int(input("enter num1"))
+b=int(input("enter num2"))
+if a==7 or b==7:
+    print("goodbay")
+if a<b:
+    print("Перше число менше другого")
+if a>b:
+    print("Перше число більше другого")
